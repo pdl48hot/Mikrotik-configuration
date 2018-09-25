fb11c3116994820c59b32a73481eeac7d8375407
dobry_kot <pdl.48.hot@gmail.com>
2018-09-25 17:09:16 +0300

    Переписан файл с классом под безопасность

--- my_class/class_security.py
+++ my_class/class_security.py
@@ -10,14 +10,22 @@ class account:
     @staticmethod
     def result(user_uid):
         my_dir = os.getcwd()
-        dir_account = my_dir + '\\accounts.yml'
-
-        try:
+        dir_account = 'C:\\Python\\Key_for_python\\accounts.yml'
+        if user_uid != 0:
             with open(dir_account, 'r') as load_account:
                 data_load = yaml.safe_load(load_account)
-                login_server = (data_load['accounts'][user_uid]['name_server'])
-                password_server = (data_load['accounts'][user_uid]['password_server'])
-        except:
-            pass
 
-        return login_server, password_server
+                login_server = (data_load['accounts'][user_uid]['name'])
+                password_server = (data_load['accounts'][user_uid]['password'])
+                return login_server, password_server
+
+        else:
+            with open(dir_account, 'r') as load_account:
+                data_load = yaml.safe_load(load_account)
+
+                login_server = (data_load['accounts'][user_uid]['name'])
+                password_server = (data_load['accounts'][user_uid]['password'])
+                port_access = (data_load['accounts'][user_uid]['port'])
+                hostname = (data_load['accounts'][user_uid]['hostname'])
+                return login_server, password_server, port_access, hostname
+
